de2655c7397d4bbfbd192a326c2a3924e70d5e69
Administrator <william@1stellar.com>
2017-03-13 18:46:15 +0800

    check device support

--- app/src/main/java/et/superflash/MainActivity.java
+++ app/src/main/java/et/superflash/MainActivity.java
@@ -5,21 +5,27 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private MaterialAnimatedSwitch aSwitch = null;
     private Camera camera = null;
-    private Camera.Parameters parameters;
+    private Camera.Parameters parameters = null;
     private String[] permissions = {Manifest.permission.CAMERA};
+    private Handler handler = null;
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,45 +40,82 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void initData() {
-        checkPermission();
+//        checkPermission();
+        initLight();
     }
 
     /**
      * check permission
      */
-    private void checkPermission() {
-        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    200);
+//    private void checkPermission() {
+//        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.CAMERA},
+//                    200);
+//        }else {
+//            initLight();
+//        }
+//    }
+
+    /**
+     * init light
+     */
+    private void initLight() {
+        aSwitch.setOnClickListener(this);
+        PackageManager pm = getPackageManager();
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+                && !pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+            camera = Camera.open();
+            parameters = camera.getParameters();
+            handler = new Handler();
+            timer = new Timer();
+        } else {
+            Toast.makeText(this,"sorry,this device do not support the app.",Toast.LENGTH_LONG).show();
+            finish();
         }
+
+//        flicker();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 200){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                aSwitch.setOnClickListener(this);
-                camera = Camera.open();
-                parameters = camera.getParameters();
-            }else{
-                Toast.makeText(MainActivity.this, "请允许权限后使用~", Toast.LENGTH_LONG).show();
-                finish();
+    private void flicker() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                switchFlash();
             }
-        }
+        },1000,100);
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if(requestCode == 200){
+//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                initLight();
+//            }else{
+//                Toast.makeText(MainActivity.this, "请允许权限后使用~", Toast.LENGTH_LONG).show();
+//                finish();
+//            }
+//        }
+//    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.mas:
-                if (parameters.getFlashMode() == Camera.Parameters.FLASH_MODE_TORCH) {
-                    closeLight();
-                } else {
-                    openLight();
-                }
+                switchFlash();
                 break;
+        }
+    }
+
+    /**
+     * switch flash
+     */
+    private void switchFlash() {
+        if (parameters.getFlashMode() == Camera.Parameters.FLASH_MODE_TORCH) {
+            closeLight();
+        } else {
+            openLight();
         }
     }
 
